524d4521e204c446a0e71cba6df36d8e3e092488
ashwani <ashwani.bhadani.3289@gmail.com>
2024-07-05 10:09:20 +0530

    CHORE: created template project

---
+++ asyncApp/src/main/java/com/tutorial/asyncApp/AsyncAppApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.asyncApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncAppApplication.class, args);
+	}
+
+}
