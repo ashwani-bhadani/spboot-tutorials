b1a7523232a67fc66577787759a8f1f7dc9a32ff
ashwani <ashwani.bhadani.3289@gmail.com>
2024-08-24 17:41:15 +0530

    FEAT: practicing webhooks

---
+++ asyncApp/src/main/java/com/tutorial/asyncApp/model/Invoice.java
@@ -0,0 +1,13 @@
+package com.tutorial.asyncApp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Invoice {
+}
